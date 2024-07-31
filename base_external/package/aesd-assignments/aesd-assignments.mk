0c36243f7c51f0313eb75a05bfbca85bbdff7c63
Kevin Flynn <2982824673@mail.nwpu.edu.cn>
2024-07-31 13:11:17 -0600

    add server application into aesd-assignment package in asiignment 5-2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '2d6433a8b146f5b793754b0f84a230ac915c4844'
+AESD_ASSIGNMENTS_VERSION = '854ac590f154ca862bf783520286905559296f93'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -17,6 +17,7 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -31,6 +32,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	sed -i -e 's/.\/writer/writer/g' $(TARGET_DIR)/usr/bin/finder-test.sh
 	sed -i -e 's/.\/finder.sh/finder.sh/g' $(TARGET_DIR)/usr/bin/finder-test.sh
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
